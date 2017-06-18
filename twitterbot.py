73bc71e8a0f1b48c3b2995f58043b55f3f41e786
Jed Davis <JDavis2759@gmail.com>
2017-06-18 14:42:40 -0500

    Ditched the AWS Lambda function and merged it into twitterbot.

--- twitterbot.py
+++ twitterbot.py
@@ -8,6 +8,13 @@ import twitter
 import config
 from markov import Markov
 
+logger = logging.getLogger()
+
+if config.DEBUG:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
 
 # The JSON dump file for last IDs seen.
 bot_data_file = config.BOT_DATA_FILE
@@ -66,7 +73,7 @@ class TwitterBot(object):
 
         # If there's room, have a chance at adding a second sentence
         if len(tweet) < 70 and random.random() < 0.65:
-            logging.info("Adding another sentence")
+            logger.info("Adding another sentence")
             tweet = tweet + " " + _add_sentence(tweet)
 
             # Sometimes add one more.
@@ -89,13 +96,13 @@ class TwitterBot(object):
 
         # Return the tweet text without posting it
         if not live_tweet:
-            logging.info("Live tweeting disabled. Returning tweet without posting.")
-            logging.info("Unposted status: %s", status)
+            logger.info("Live tweeting disabled. Returning tweet without posting.")
+            logger.info("Unposted status: %s", status)
             return status
 
         # Post the status
         status = self.api.PostUpdate(status, in_reply_to_status_id=reply_to)
-        logging.info(status.text.encode('utf-8'))
+        logger.info(status.text.encode('utf-8'))
 
     @staticmethod
     def clean_data(tweet_data):
@@ -119,8 +126,8 @@ class TwitterBot(object):
         Dump tweet ID data to json.
         """
         with open(bot_data_file, 'w') as f:
-            logging.info("Dumping last id seen to json: %s", self.last_id_seen)
-            logging.info("Dumping reply last id seen to json: %s", self.last_reply_id_seen)
+            logger.info("Dumping last id seen to json: %s", self.last_id_seen)
+            logger.info("Dumping reply last id seen to json: %s", self.last_reply_id_seen)
             json.dump({"last_id_seen": self.last_id_seen,
                        "last_reply_id_seen": self.last_reply_id_seen}, f)
 
@@ -141,11 +148,11 @@ class TwitterBot(object):
 
         # Get any new replies or mentions
         replies = self.api.GetMentions(since_id=self.last_reply_id_seen)
-        logging.info(replies)
+        logger.info(replies)
 
         # Return None if there are no new replies
         if replies == []:
-            logging.info("No new replies to respond to.")
+            logger.info("No new replies to respond to.")
             return replies
 
         # Update the bot with the latest reply ID seen
@@ -168,11 +175,11 @@ class TwitterBot(object):
                                           trim_user=True)
 
         if not tweets:
-            logging.info("Database is up to date with the latest ID seen.")
+            logger.info("Database is up to date with the latest ID seen.")
             return
 
         self.last_id_seen = tweets[0].id
-        logging.info("Setting last_id_seen to: %s", self.last_id_seen)
+        logger.info("Setting last_id_seen to: %s", self.last_id_seen)
 
         def _tweet_data_gen(tweets):
             for status in tweets:
@@ -181,3 +188,28 @@ class TwitterBot(object):
                     yield word
 
         self.markov.update_db(_tweet_data_gen(tweets))
+
+if __name__ == '__main__':
+    # Initialize the bot and update the database with any new tweets.
+    logger.info("Initializing TwitterBot")
+    bot = TwitterBot()
+    logger.info("Updating tweet database.")
+    bot.update_tweet_database()
+
+    # Have a chance at posting a new tweet.
+    roll = random.randrange(config.TWEET_ODDS)
+    if roll == 0:
+        logger.info("Rolled %s. Posting a new tweet", roll)
+        bot.new_tweet()
+    else:
+        logger.info("Rolled %s. Not posting this time.", roll)
+
+    # Respond to any recent replies or mentions.
+    logger.info("Responding to new tweets and mentions.")
+    bot.reply_tweets()
+
+    # Dump update information to JSON.
+    logger.info("Dumping bot data to JSON")
+    bot.dump_data()
+
+    logger.info("All finished.")
